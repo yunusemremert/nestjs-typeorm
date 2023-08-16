b8e852fac23f4701ea7cbe169017bcaa86b366b8
Yunus Emre MERT <yunusemremert@yandex.com>
2023-08-16 14:54:02 +0300

    edited item entity

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,1 +1,17 @@
-export class Item {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Item {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ default: true })
+  public: boolean;
+
+  constructor(item: Partial<Item>) {
+    Object.assign(this, item);
+  }
+}
