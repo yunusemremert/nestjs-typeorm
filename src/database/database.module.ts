e7c8dd803bf0b435ce60e8cc4f7566f0a751e957
Yunus Emre MERT <yunusemremert@yandex.com>
2023-08-16 14:53:40 +0300

    fix database port

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -8,7 +8,7 @@ import { ConfigService } from '@nestjs/config';
       useFactory: (configService: ConfigService) => ({
         type: 'mysql',
         host: configService.getOrThrow('MYSQL_HOST'),
-        port: configService.getOrThrow('MYSQL_PORT'),
+        port: +configService.getOrThrow('MYSQL_PORT'),
         database: configService.getOrThrow('MYSQL_DATABASE'),
         username: configService.getOrThrow('MYSQL_USERNAME'),
         password: configService.getOrThrow('MYSQL_PASSWORD'),
